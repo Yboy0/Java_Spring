6c318d18d962c6aec9e50cbf45fa21c5d98b1de1
Oo-Uu <fbduddn97@naver.com>
2021-01-17 21:23:37 +0900

    OrderGroup Api 작성 및 오타 발생 할 수 있는 user item status와 ordrGroup orderType enum class로 대체

--- study/src/main/java/com/example/demo/model/entity/User.java
+++ study/src/main/java/com/example/demo/model/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.demo.model.entity;
 
+import com.example.demo.model.enumclass.UserStatus;
 import lombok.*;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.CreatedBy;
@@ -30,7 +31,8 @@ public class User {
     //@Column(name="email")
     private String password;
 
-    private String status;
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
 
     private String email;
 
---
+++ study/src/main/java/com/example/demo/service/OrderGroupApiLogicService.java
@@ -0,0 +1,117 @@
+package com.example.demo.service;
+
+import com.example.demo.ifs.CrudInterface;
+import com.example.demo.model.entity.OrderGroup;
+import com.example.demo.model.network.Header;
+import com.example.demo.model.network.request.OrderGroupApiRequest;
+import com.example.demo.model.network.response.OrderGroupApiResponse;
+import com.example.demo.repository.OrderGroupRepository;
+import com.example.demo.repository.UserRepository;
+import jdk.javadoc.internal.doclets.formats.html.markup.Head;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+
+    @Autowired
+    private OrderGroupRepository  orderGroupRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public Header<OrderGroupApiResponse> create(Header<OrderGroupApiRequest> request) {
+
+        OrderGroupApiRequest body = request.getData();
+
+        OrderGroup orderGroup = OrderGroup.builder()
+                .status(body.getStatus())
+                .orderType(body.getOrderType())
+                .revAddress(body.getRevAddress())
+                .revName(body.getRevName())
+                .paymentType(body.getPaymentType())
+                .totalprice(body.getTotalPrice())
+                .totalQuantity(body.getTotalQuantity())
+                .orderAt(LocalDateTime.now())
+                .user(userRepository.getOne(body.getUserId()))
+                .build();
+
+        OrderGroup newOrderGroup = orderGroupRepository.save(orderGroup);
+
+        return response(newOrderGroup);
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> read(Long id) {
+
+       return orderGroupRepository.findById(id)
+                .map(this::response)
+                .orElseGet(()->Header.Error("데이터 없음"));
+    }
+
+    @Override
+    public Header<OrderGroupApiResponse> update(Header<OrderGroupApiRequest> request) {
+
+        OrderGroupApiRequest body = request.getData();
+
+        return orderGroupRepository.findById(body.getId())
+                .map(orderGroup -> {
+
+                    orderGroup
+                            .setStatus(body.getStatus())
+                            .setOrderType(body.getOrderType())
+                            .setRevAddress(body.getRevAddress())
+                            .setRevName(body.getRevName())
+                            .setPaymentType(body.getPaymentType())
+                            .setTotalprice(body.getTotalPrice())
+                            .setTotalQuantity(body.getTotalQuantity())
+                            .setOrderAt(body.getOrderAt())
+                            .setArrivalDate(body.getArrivalDate())
+                            .setUser(userRepository.getOne(body.getUserId()))
+                            ;
+                    return orderGroup;
+                })
+                .map(changeOrderGroup -> orderGroupRepository.save(changeOrderGroup))
+                .map(newOrderGroup -> response(newOrderGroup))
+                .orElseGet(()-> Header.Error("데이터 없음"));
+
+
+    }
+
+    @Override
+    public Header delete(Long id) {
+
+        orderGroupRepository.findById(id)
+                .map(orderGroup -> {
+                    orderGroupRepository.delete(orderGroup);
+
+                    return Header.OK();
+                })
+                .orElseGet(()->Header.Error("데이터 없음"));
+        return null;
+    }
+
+    private Header<OrderGroupApiResponse> response(OrderGroup orderGroup){
+        OrderGroupApiResponse body = OrderGroupApiResponse.builder()
+                .id(orderGroup.getId())
+                .status(orderGroup.getStatus())
+                .orderType(orderGroup.getOrderType())
+                .revAddress(orderGroup.getRevAddress())
+                .revName(orderGroup.getRevName())
+                .paymentType(orderGroup.getPaymentType())
+                .totalPrice(orderGroup.getTotalprice())
+                .totalQuantity(orderGroup.getTotalQuantity())
+                .orderAt(orderGroup.getOrderAt())
+                .arrivalDate(orderGroup.getArrivalDate())
+                .userId(orderGroup.getUser().getId())
+                .build();
+        return Header.OK(body);
+    }
+}
